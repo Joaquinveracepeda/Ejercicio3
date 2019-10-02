7ede63453688a4d3b77e9cc8785acd14feb65c22
vn0cmbh <vn0cmbh@m-c02yp61elvcg.local>
2019-10-02 09:52:17 -0300

    sumatorias

---
+++ src/clase/Main.java
@@ -0,0 +1,17 @@
+package clase;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el seundo numero"));
+
+        Operaciones op = new Operaciones();
+
+        System.out.println("la suma es: "+ op.sumar(n1, n2));
+        System.out.println("La multiplicacion es: "+ op.multiplicar(n1, n2));
+        System.out.println("La division es: "+ op.dividir(n1, n2));
+    }
+}
---
+++ src/clase/Operaciones.java
@@ -0,0 +1,26 @@
+package clase;
+public class Operaciones {
+
+    // write your code here
+
+    public int sumar(int numero1, int numero2) {
+        int suma = numero1 + numero2;
+        return suma;
+    }
+
+    public int multiplicar(int numero1, int numero2) {
+        int multiplicacion = numero1 * numero2;
+        return multiplicacion;
+    }
+
+    public int dividir(int numero1, int numero2) {
+        int division = numero1 / numero2;
+        return division;
+
+    }
+
+}
+
+
+
+
